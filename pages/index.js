2e444e525b4fee91cbe364d8e4b1fa65da5cd366
Alessandro <afurukawa86@gmail.com>
2020-12-17 21:14:37 -0300

    incluído logo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
-function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+import Head from 'next/head'
+import styles from '../styles/Index.module.css'
+import Logo from '../components/Logo/Logo'
+import Categoria from '../components/Categoria/Categoria'
 
-export default Home
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>kakeibo</title>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
+        <meta name='robots' content='index/follow' />
+        <meta name='description' content='controle financeiro pessoal' />
+      </Head>
+
+      <div className={styles.left}>
+        <div className={styles.left__logo}>
+          <Logo />
+        </div>
+        <section className={styles.left__categoria}>
+          <Categoria />
+        </section>
+      </div>
+
+      <div className={styles.right}>
+        <nav className={styles.right__nav}>
+          nav
+        </nav>
+        <main className={styles.right__conteudo}>
+          conteudo
+        </main>
+      </div>
+    </div>
+  )
+}
